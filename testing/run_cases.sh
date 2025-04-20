50c20c2e68fc3eec29b29ba484488974cffb29a3
vrcoelho <tmospheric@gmail.com>
2025-04-20 20:29:54 -0300

    test: improve script

--- testing/run_cases.sh
+++ testing/run_cases.sh
@@ -5,6 +5,8 @@
 # redirects > /dev/null to the file, emptying it
 > testcases_output
 
+ok=0
+err=0
 for case in ./cases/etapa2/asl*
 do
     filename=$(basename "$case")
@@ -20,9 +22,8 @@ do
     # executa nosso programa
     echo "===================" >> testcases_output
     echo "TESTE $case" >> testcases_output
-
     # 2>&1 manda tanto o stderr quanto stdout
-    ./../../compilers/parser < "$case" >> testcases_output 2>&1
+    ./../../compilers/etapa2 < "$case" >> testcases_output 2>&1
 
     # captura resultado aqui, logo apos execucao
     result=$?
@@ -31,13 +32,31 @@ do
     echo "" >> testcases_output
 
 
-    
-
     # se o retorno do codigo c foi 0 (sucesso)
     if [ "$result" -eq "$return_expected" ]
     then
-        echo "caso $filename: OK"
+        # echo "caso $filename: OK"
+        # se esperava ser sucesso
+        if [ "$result" -eq 0 ]
+        then
+            :
+            # echo "caso $filename: OK"
+        fi
+        ok=$((ok + 1))
     else
-        echo "              caso $filename: ERROR"
-    fi
+        # se esperava ser error
+        # if [ "$return_expected" -eq 1 ]
+
+
+        # se esperava ser aceito
+        if [ "$return_expected" -eq 0 ]
+        then
+            echo "              caso $filename: ERROR"
+            :
+        fi
+        err=$((err + 1))
+    fi  
 done
+
+echo "ok: $ok"
+echo "error: $err"
