751ff5982fe5101e83fb0b8fd27e1bc24b5e89d4
vrcoelho <tmospheric@gmail.com>
2025-05-08 19:19:42 -0300

    dot command was outputting a lot of crap
    fix this in future?

---
+++ testing/e3_run_cases.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# arquivo de testes para testar todos os teste cases
+
+# redirects > /dev/null to the file, emptying it
+> testcases_output
+
+ok=0
+err=0
+for case in ./cases/etapa2/asl*
+do
+    filename=$(basename "$case")
+    # echo "EXECUTANDO $filename"
+
+    return_expected=0    
+    line=$(head -n 1 "$case")
+    if [ "$line" != "//CORRECT" ]
+    then
+        return_expected=1
+    fi
+
+    # executa nosso programa
+
+    # echo "===================" >> testcases_output
+
+    # echo "TESTE $case" >> testcases_output
+    touch ./cases/etapa3/results/"$filename"
+    # 2>&1 manda tanto o stderr quanto stdout
+    ./../../compilers/etapa3 < "$case" >> ./cases/etapa3/results/"$filename" 2>&1
+
+    # captura resultado aqui, logo apos execucao
+    result=$?
+
+    # echo "\n===================" >> testcases_output
+    # echo "" >> testcases_output
+
+
+    # se o retorno do codigo c foi igual ao esperado
+    if [ "$result" -eq "$return_expected" ]
+    then
+        # echo "caso $filename: OK"
+        # se esperava ser sucesso
+        if [ "$result" -eq 0 ]
+        then
+            :
+            # echo "caso $filename: OK"
+
+
+            # posso criar o png da arvore gerada
+            dot -Tpng ./cases/etapa3/results/"$filename" -o ./cases/etapa3/results/"$filename".png > /dev/null 2>&1
+
+        fi
+        ok=$((ok + 1))
+
+        if [ "$result" -eq 1 ]
+        then
+            :
+            # se era erro posso apagar o arquivo
+            # rm touch ./cases/etapa3/results/"$filename"
+        fi
+    else
+        # se esperava ser error
+        # if [ "$return_expected" -eq 1 ]
+
+
+        # se esperava ser aceito
+        if [ "$return_expected" -eq 0 ]
+        then
+            echo "              caso $filename: ERROR"
+            :
+        fi
+        err=$((err + 1))
+    fi  
+done
+
+echo "ok: $ok"
+echo "error: $err"
