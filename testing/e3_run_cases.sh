55d385dfd3ea9b9970bab48c3081600a4663ecf5
vrcoelho <tmospheric@gmail.com>
2025-05-15 16:10:33 -0300

    testing: adapt script, include tests for etapa2
    - adapt script for new folder
    - run tests for etapa2 cases that parse correctly

--- testing/e3_run_cases.sh
+++ testing/e3_run_cases.sh
@@ -5,13 +5,74 @@
 # redirects > /dev/null to the file, emptying it
 > testcases_output
 
+rm ./cases/etapa3/results/*
+
 ok=0
 err=0
 eq=0
 neq=0
 
+#########################
+#### testes etapa3
+#########################
 
-for case in ./cases/etapa3/z*
+# run testes da etapa2
+for case in ./cases/etapa2/asl*
+do
+    filename=$(basename "$case")
+
+    return_expected=0    
+    line=$(head -n 1 "$case")
+    if [ "$line" != "//CORRECT" ]
+    then
+        return_expected=1
+    fi
+
+    # se espero que va funcionar
+    if [ "$return_expected" -eq 0 ]
+    then
+        touch ./cases/etapa3/results/"$filename"
+        > ./cases/etapa3/results/"$filename"
+        # 2>&1 manda tanto o stderr quanto stdout
+        ./../../compilers/etapa3 < "$case" >> ./cases/etapa3/results/"$filename" 2>&1
+
+        # captura resultado aqui, logo apos execucao
+        result=$?
+
+        # se o retorno do codigo c foi igual ao esperado
+        if [ "$result" -eq "$return_expected" ]
+        then
+            dot -Tpng ./cases/etapa3/results/"$filename" -o ./cases/etapa3/results/"$filename".png > /dev/null 2>&1
+            result2=$?
+            if [ "$result2" -ne 0 ]
+            then
+                echo "                  > case $filename: nao gerou arvore"
+                err=$((err + 1))
+            else
+                ok=$((ok + 1))
+            fi
+        else
+            # ou seja, deu algum erro...
+            echo "                  > caso $filename: leak"
+            err=$((err + 1)) 
+        fi
+    fi
+done
+
+echo "====="
+echo "cases from etapa2:"
+echo "====="
+echo "ok: $ok"
+echo "error: $err"
+
+#########################
+#### testes etapa3
+#########################
+
+ok=0
+err=0
+
+for case in ./cases/etapa3/professor/z*
 do
     filename=$(basename "$case")
     # echo "EXECUTANDO $filename"
@@ -20,7 +81,7 @@ do
     then
         # resposta a gerar gabarito
 
-        dot -Tpng ./cases/etapa3/"$filename" -o ./cases/etapa3/results/"$filename"_resposta.png > /dev/null 2>&1
+        dot -Tpng ./cases/etapa3/professor/"$filename" -o ./cases/etapa3/results/"$filename"_resposta.png > /dev/null 2>&1
     else
         # caso de teste per se
     
@@ -53,7 +114,7 @@ do
             
 
             # como deu certo, verifico se as arvores geradas sao iguais
-            python3 compare_trees.py ./cases/etapa3/results/"$filename" ./cases/etapa3/"$filename".ref.dot
+            python3 compare_trees.py ./cases/etapa3/results/"$filename" ./cases/etapa3/professor/"$filename".ref.dot
 
             result2=$?
             if [ "$result2" -eq 0 ]
