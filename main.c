5d8fbc372dd6ef8b43a74751abe789c95730a3a0
vrcoelho <tmospheric@gmail.com>
2025-05-25 23:28:24 -0300

    feat: include some of the needed semantic checks
    - implements the checking if identifiers were declared
    - should be able to pick up the following errors:
    ERR_UNDECLARED
    ERR_DECLARED
    ERR_VARIABLE
    ERR_FUNCTION
    ERR_MISSING_ARGS
    ERR_EXCESS_ARGS
    
    - with the exception of the scope for the if and while commands, that
    should receive a new scope each, still to do
    
    - the type is still not being checked, the following errors
    are still to be recognized:
    
    ERR_WRONG_TYPE
    ERR_WRONG_TYPE_ARGS

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include "asd.h"
+#include "internals.h"
 extern int yyparse(void);
 extern int yylex_destroy(void);
 extern int yydebug;
 asd_tree_t *arvore = NULL;
+stack_symbol_table* stack_of_tables = NULL;
+
+int n_args = -1;
+int n_args_on_call = -1;
+
 int main(int argc, char **argv)
 {
+  stack_of_tables = new_stack_of_tables();
+  root_symbol_table* global_table = new_symbol_table();
+  register_table_to_stack(stack_of_tables, global_table);
   // yydebug = 1;
   int ret = yyparse();
   asd_print_graphviz(arvore);
