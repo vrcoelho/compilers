c65f8aa259522b552d12071640d12c92f28b8583
vrcoelho <tmospheric@gmail.com>
2025-05-25 23:27:23 -0300

    feat: include test file for data structures

--- test_internals.c
+++ test_internals.c
@@ -80,9 +80,53 @@ int main() {
 
     printf("%s\n", "hello");
 
+
+
+    // new testing
+
+
+    
+    stack_symbol_table* stack = new_stack_of_tables();
+    register_table_to_stack(stack, table);
+
+
+
+
+    // creating a second table
+    root_symbol_table* table2 = new_symbol_table();
+    char newfuncname3[] = "variable_da_tabela2";
+    int functype3 = 0;
+    register_variable_to_tableofc(table2, newfuncname3, functype3);
+
+    printf("%s\n", "hello2");
+    register_table_to_stack(stack, table2);
+    print_stack_of_tables(stack);
+    // adding the second t
+
+
+    // prints only the current
+    printf("%s\n", "hello3here");
+    print_table(stack->current_table);
+
+
+
+    // search for the variable?
+    int r = search_variable_on_stack(stack, "second_function");
+    printf("achou?: %d\n", r);
+
+
     free_symbol_table_contents(table);
     free(table);
-    // new testing
+
+    
+
+    free_symbol_table_contents(table2);
+    free(table2);
+
+    free(stack);
+
+
+
 
     return 0;
 }
