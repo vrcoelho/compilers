a2105e696a03a72d585ff7f120ca5696bf3e5cd8
vrcoelho <tmospheric@gmail.com>
2025-05-25 23:25:38 -0300

    feat: include missing functions for data structures
    - implements functions to:
        - search functions for variables and functions in tables and on the stack
        - check n args
        - free structures after use

--- internals.c
+++ internals.c
@@ -34,13 +34,6 @@ void svalor_lexico_free(svalor_lexico_st *v)
 }
 
 
-
-
-
-// added functions
-
-
-
 element_symbol_table* new_entry_variable(const char* name) {
     element_symbol_table* p = NULL;
     p = calloc(1, sizeof(element_symbol_table));
@@ -86,29 +79,6 @@ element_symbol_table* new_entry_function(
 }
 
 
-
-// ===========================
-
-// i need to actually have a table with the entries
-// based on its name I need to search for a 
-
-
-// a table has a list of entrys
-// and a pointer to its parent table? 
-//   |-> not sure we actually need it
-//   |-> because we will have a `stack` with pointers to the tables
-
-
-
-
-
-
-
-
-
-// i am like setting the pointers but I actually need
-// to create the pointers, I believe. and set it up
-// adiciona ao ultimo da tabela?
 void insert_to_table(
     root_symbol_table* table_root,
     element_symbol_table* element) {
@@ -138,15 +108,6 @@ void insert_to_table(
 }
 
 
-
-
-
-
-
-
-
-// currently used functions
-
 void free_entry_st(element_symbol_table* st) {
     if (st == NULL) {
         // error?
@@ -162,12 +123,40 @@ void print_table(root_symbol_table* table_root)
     element_symbol_table* nextp = table_root->header;
     while (nextp != NULL)
     {
-        printf("%s\n", nextp->name);
+        char rtype[255];
 
+        if (nextp->type_or_return == integer)
+            strcpy(rtype, "int");
+        else if(nextp->type_or_return == floatpoint)
+            strcpy(rtype, "float");            
+        
+        if (nextp->type_of_element == function)
+        {
+            printf("%s %s %s with %d args\n", 
+                "function", 
+                nextp->name,
+                rtype,
+                nextp->n_args);
+        }
+        else if(nextp->type_of_element == variable)
+        {
+            printf("%s %s %s\n", 
+                "variable", 
+                nextp->name,
+                rtype);
+        }
+        else
+        {
+            // error
+            printf("%s %d %s %s\n", 
+                "ERROR???? UNDEFINED", 
+                nextp->type_or_return,
+                nextp->name,
+                rtype);
+        }
         nextp = nextp->next;
     }
         
-
 }
 
 
@@ -187,7 +176,7 @@ void free_symbol_table_contents(root_symbol_table* table_root) {
         // error? print something?
         return;
     }
-
+    // print_table(table_root);
     element_symbol_table* curr;
     element_symbol_table* next;
     curr = table_root->header;
@@ -196,7 +185,6 @@ void free_symbol_table_contents(root_symbol_table* table_root) {
     while (!finished) {        
         free_entry_st(curr);
         free(curr);
-
         if (next == NULL)
             finished = 1;
         else
@@ -206,6 +194,26 @@ void free_symbol_table_contents(root_symbol_table* table_root) {
         }
     }
 
+}
+
+void free_current_table(stack_symbol_table* stack ) {
+    if (stack == NULL ) { 
+        // error because we need a valid pointer
+        // printf("ERRO STACK ERA VAZIA");
+        return;
+    }
+
+    if (stack->current_table == NULL ) { 
+        // error because we need a valid pointer
+        // printf("ERRO current_table ERA VAZIA");
+        return;
+    }
+
+    root_symbol_table* to_free = stack->current_table;
+    stack->current_table = stack->current_table->mother_table;
+    stack->current_table->next_table = NULL;
+    free_symbol_table_contents(to_free);
+    free(to_free);
 }
 
 element_symbol_table* new_entry_variable2(
@@ -226,7 +234,6 @@ element_symbol_table* new_entry_variable2(
     return p;
 }
 
-
 void register_variable_to_tableofc(
     root_symbol_table* table_root,
     const char* variable_name,
@@ -241,15 +248,442 @@ void register_variable_to_tableofc(
         table_root->header = new_entry_variable2(variable_name, variable_type);
         return;
     }
-    printf("i got here\n");
     element_symbol_table* nextp = table_root->header;
     while (nextp->next != NULL)
     {
         nextp = nextp->next;
-        printf("a\n");
     }
 
-    
     nextp->next = new_entry_variable2(variable_name, variable_type);
     nextp->next->next = NULL; // just to make sure
+}
+
+// register functions to table
+element_symbol_table* new_entry_function2(
+    const char* name, 
+    int return_type,
+    int nargs) {
+    element_symbol_table* p = NULL;
+    p = calloc(1, sizeof(element_symbol_table));
+    if (p != NULL) {
+        p->type_of_element = function;
+        p->name = strdup(name);
+        p->next = NULL;
+
+        p->type_or_return = return_type;
+        p->n_args = nargs;
+        // TODO INCLUDE PARAMETERS LIST IT HERE
+        p->parameters_list = NULL;
+        
+    }
+    return p;
+}
+
+void register_function_to_tableofc(
+    root_symbol_table* table_root,
+    const char* name, 
+    int return_type,
+    int nargs) {
+
+    if (table_root == NULL ) { 
+        // error because we need a valid pointer
+        return;
+    }
+
+    if (table_root->header == NULL ) { 
+        table_root->header = new_entry_function2(name, return_type, nargs);
+        return;
+    }
+    element_symbol_table* nextp = table_root->header;
+    while (nextp->next != NULL)
+    {
+        nextp = nextp->next;
+    }
+    
+    nextp->next = new_entry_function2(name, return_type, nargs);
+    nextp->next->next = NULL; // just to make sure
+}
+
+
+// stack functions
+
+stack_symbol_table* new_stack_of_tables() {
+    stack_symbol_table* stack = NULL;
+    stack = calloc(1, sizeof(stack_symbol_table));
+    if (stack == NULL) {
+        // error end of the world
+    }
+    stack->first_table = NULL;
+    stack->current_table = NULL;
+    return stack;
+
+}
+
+void register_table_to_stack(
+    stack_symbol_table* stack, 
+    root_symbol_table* table    
+) {
+    if (stack == NULL) {
+        // error we need a valid pointer
+        return;
+    }
+
+    if (table == NULL) {
+        // error we need a valid pointer
+        return;
+    }
+
+
+    if (stack->first_table == NULL){
+        stack->first_table = table;
+        stack->current_table = table;
+    } else {
+        root_symbol_table* nextable = stack->first_table;
+        while (nextable->next_table != NULL)
+        {
+            nextable = nextable->next_table;
+        }
+        nextable->next_table = table;
+        table->mother_table = nextable;
+        stack->current_table = table;
+    }
+}
+   
+
+void print_stack_of_tables( stack_symbol_table* stack) {
+    if (stack == NULL) {
+        // error we need a valid pointer
+        return;
+    }
+
+    root_symbol_table* curr_table = stack->first_table;
+    while(curr_table != NULL) { 
+        print_table(curr_table);
+        curr_table = curr_table->next_table;
+    } 
+    
+
+}
+
+// function that searchs for a variable going upwards on the tables
+
+int search_variable_on_table(
+    root_symbol_table* table,
+    const char* varname
+) {
+    if (table == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (varname == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (table->header == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+    // comeco a procurar na tabela
+    element_symbol_table* curr = table->header;
+    while(curr != NULL)
+    {
+        // checa se o nome bate
+        if (strcmp(varname, curr->name) == 0)
+        {
+            // the name matches
+            // was it really a variable?
+            if (curr->type_of_element == variable)
+                return 1;
+            else
+                return 3;
+        }
+        curr = curr->next;
+    }
+    // not found
+    return -1;
+}
+
+
+int search_variable_on_stack(
+    stack_symbol_table* stack_of_tables,
+    const char* varname
+) {
+    if (stack_of_tables == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (stack_of_tables->current_table == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    root_symbol_table* curr_table = stack_of_tables->current_table;
+
+    int i = 1;
+    while(curr_table != NULL) {
+        int r = search_variable_on_table(curr_table, varname);
+        if (r == 1 | r == 3)
+            return r;
+        i++;
+        curr_table = curr_table->mother_table;
+    }
+    return -1;
+}
+
+
+int free_stack_and_all_tables(
+    stack_symbol_table* stack_of_tables
+) {
+    if (stack_of_tables == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (stack_of_tables->current_table == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    root_symbol_table* curr_table = stack_of_tables->current_table;
+
+    int i = 1;
+    while(curr_table != NULL) {
+        // printf("Cleaning table: %d\n", i);
+        root_symbol_table* next_to_free = curr_table->mother_table;
+        free_symbol_table_contents(curr_table);
+        curr_table = next_to_free;
+        i++;
+    }
+    return 1;
+}
+
+
+
+int search_function_on_table(
+    root_symbol_table* table,
+    const char* funcName
+) {
+    if (table == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (funcName == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (table->header == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+    element_symbol_table* curr = table->header;
+    while(curr != NULL)
+    {
+        if (strcmp(funcName, curr->name) == 0)
+        {
+            // the name matches
+            // was it really a function?
+            if (curr->type_of_element == function)
+                return 1; // found
+            else
+                return 5; // it was a variable
+        }
+
+        curr = curr->next;
+    }
+
+    // not found
+    return -1;
+}
+
+
+int search_function_on_stack(
+    stack_symbol_table* stack_of_tables,
+    const char* funcName
+) {
+    if (stack_of_tables == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (stack_of_tables->current_table == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    root_symbol_table* curr_table = stack_of_tables->current_table;
+
+    int i = 1;
+    while(curr_table != NULL) {
+        // printf("Searching on table: %d\n", i);
+        int r = search_function_on_table(curr_table, funcName);
+        if (r == 1 | r == 5)
+            return r;
+        i++;
+        curr_table = curr_table->mother_table;
+    }
+    return -1;
+}
+
+
+/// checking args
+
+int check_args_function_on_table(
+    root_symbol_table* table,
+    const char* funcName,
+    int received_args
+) {
+    if (table == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (funcName == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (table->header == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+    // comeco a procurar na tabela
+    
+    element_symbol_table* curr = table->header;
+    while(curr != NULL)
+    {
+        if (strcmp(funcName, curr->name) == 0)
+        {
+            // the name matches
+            // was it really a function?
+            if (curr->type_of_element == function)
+            {
+                if (received_args == curr->n_args)
+                // checking the n args
+                    return 1; // tudo certo
+                else if (received_args < curr->n_args)
+                    return 11;
+                else if (received_args > curr->n_args)
+                    return 22;
+            }
+            else
+                return 5; // it was a variable
+        }
+
+        curr = curr->next;
+    }
+
+    // not found
+    return -1;
+}
+
+
+int check_args_function_on_stack(
+    stack_symbol_table* stack_of_tables,
+    const char* funcName,
+    int received_args
+) {
+    if (stack_of_tables == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (stack_of_tables->current_table == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    root_symbol_table* curr_table = stack_of_tables->current_table;
+
+    int i = 1;
+    while(curr_table != NULL) {
+        int r = check_args_function_on_table(
+            curr_table,
+            funcName,
+            received_args);
+        if (r != -1)
+            return r;
+        i++;
+        curr_table = curr_table->mother_table;
+    }
+    return -1;
+}
+
+
+
+
+
+
+
+
+
+
+// to find if something was already declared
+int search_name_taken_on_table(
+    root_symbol_table* table,
+    const char* name
+) {
+    if (table == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (name == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (table->header == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+    // comeco a procurar na tabela
+    element_symbol_table* curr = table->header;
+    while(curr != NULL)
+    {
+        // checa se o nome bate
+
+        if (strcmp(name, curr->name) == 0)
+        {
+                return 1;
+        }
+
+        curr = curr->next;
+    }
+
+    // not found
+    return -1;
+}
+
+
+int search_name_taken_on_stack(
+    stack_symbol_table* stack_of_tables,
+    const char* name
+) {
+    if (stack_of_tables == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    if (stack_of_tables->current_table == NULL) {
+        // error we need a valid pointer
+        return -2;
+    }
+
+    root_symbol_table* curr_table = stack_of_tables->current_table;
+
+    int i = 1;
+    while(curr_table != NULL) {
+        // printf("Searching on table: %d\n", i);
+        int r = search_name_taken_on_table(curr_table, name);
+        if (r == 1)
+            return r;
+        i++;
+        curr_table = curr_table->mother_table;
+    }
+    return -1;
 }
--- internals.h
+++ internals.h
@@ -13,10 +13,8 @@ svalor_lexico_st *svalor_lexico_new(
 void svalor_lexico_free(svalor_lexico_st *v);
 
 
-// we need a new structure
-
-typedef enum {variable, function} variable_or_function;
-typedef enum {integer, floatpoint} type_of_element;
+typedef enum {variable = 55, function = 66} variable_or_function;
+typedef enum {integer = 77, floatpoint = 88} type_of_element;
 
 typedef struct element_symbol_table_st
 {
@@ -42,27 +40,31 @@ typedef struct root_symbol_table_st
 {
     // points to the next symbol
     struct element_symbol_table_st* header;
+    struct root_symbol_table_st* mother_table;
+    struct root_symbol_table_st* next_table;
 } root_symbol_table;
 
+
+typedef struct stack_symbol_table_st {
+    struct root_symbol_table_st* first_table;
+    struct root_symbol_table_st* current_table;
+} stack_symbol_table;
+
+
+
+// probably should remove by now
 element_symbol_table* new_entry_variable(const char* name);
+
 element_symbol_table* new_entry_function(
     const char* name, 
     int nargs, 
     type_of_element * argslist);
-
-
 
 void insert_to_table(
     root_symbol_table* table_root,
     element_symbol_table* element);
 
 
-
-
-
-
-
-// really used functions
 
 void free_entry_st(element_symbol_table* st);
     
@@ -79,3 +81,71 @@ void register_variable_to_tableofc(
     root_symbol_table* table_root,
     const char* variable_name,
     int variable_type);
+
+void register_function_to_tableofc(
+    root_symbol_table* table_root,
+    const char* name, 
+    int return_type,
+    int nargs);
+
+stack_symbol_table* new_stack_of_tables();
+void register_table_to_stack(
+    stack_symbol_table* stack, 
+    root_symbol_table* table    
+);
+
+void print_stack_of_tables( stack_symbol_table* stack);
+
+
+int search_variable_on_table(
+    root_symbol_table* table,
+    const char* varname
+);
+
+int search_variable_on_stack(
+    stack_symbol_table* stack_of_tables,
+    const char* varname
+);
+
+
+void free_current_table(
+    stack_symbol_table* stack );
+
+int free_stack_and_all_tables(
+    stack_symbol_table* stack_of_tables
+);
+
+
+int search_function_on_table(
+    root_symbol_table* table,
+    const char* funcName
+);
+
+int search_function_on_stack(
+    stack_symbol_table* stack_of_tables,
+    const char* funcName
+);
+
+
+int check_args_function_on_table(
+    root_symbol_table* table,
+    const char* funcName,
+    int received_args
+);
+
+int check_args_function_on_stack(
+    stack_symbol_table* stack_of_tables,
+    const char* funcName,
+    int received_args
+) ;
+
+
+int search_name_taken_on_table(
+    root_symbol_table* table,
+    const char* name
+);
+
+int search_name_taken_on_stack(
+    stack_symbol_table* stack_of_tables,
+    const char* name
+);
